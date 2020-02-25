5fdb9169fbb0b837534f4ee5bced84c29bcc0fd9
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-02-25 19:15:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
+  if (array == undefined){
+    return 0;
+  }
+  if (array.length != 0){
+    return Math.min.apply(null, array);
+  } else {
   return 0;
+  }
 }
 
 exports.max = function max (array) {
+  if (array == undefined){
+    return 0;
+  }
+  if (array.length != 0){
+    return Math.max.apply(null, array);
+  } else {
   return 0;
+  }
 }
 
 exports.avg = function avg (array) {
+  if (array == undefined){
+    return 0;
+  }
+  if (array.length != 0){
+    return array.reduce((accumulator, currentNumber) => accumulator + currentNumber) / array.length;
+  } else {
   return 0;
+  }
 }
